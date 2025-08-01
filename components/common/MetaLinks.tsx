d81e03db71e90248d81dfd6929d35d800a075213
fox3000foxy <fox3000foxy@gmail.com>
2025-08-01 12:44:19 +0200

    refactor: remove unused attributes from Font Awesome stylesheet link

--- components/common/MetaLinks.tsx
+++ components/common/MetaLinks.tsx
@@ -52,9 +52,6 @@ export default function () {
             <link
                 rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-                integrity="sha512-..."
-                crossOrigin="anonymous"
-                referrerPolicy="no-referrer"
             />
         </>
     );
