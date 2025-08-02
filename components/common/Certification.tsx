eb726480eb7cd70a52598d7e5585f42c84a27d86
fox3000foxy <fox3000foxy@gmail.com>
2025-08-02 22:42:58 +0200

    feat: refactor Certification component to use useEffect for markSrc state management

--- components/common/Certification.tsx
+++ components/common/Certification.tsx
@@ -1,40 +1,41 @@
+import React from "react";
 import CachedImage from "../utils/CachedImage";
 export interface ShopItem {
-  itemId: string;
-  name: string;
-  description: string;
-  price: number;
-  stock?: number; // optionnel, si le backend le fournit,
-  iconHash: string;
+    itemId: string;
+    name: string;
+    description: string;
+    price: number;
+    stock?: number; // optionnel, si le backend le fournit,
+    iconHash: string;
 }
 interface User {
-  verified: boolean;
-  id: string;
-  username: string;
-  disabled?: boolean;
-  admin?: boolean;
-  isStudio?: boolean;
-  inventory?: ({ itemId: string; name: string; description: string; price: number; iconHash: string; } & { amount: number })[];
-  ownedItems?: ShopItem[];
+    verified: boolean;
+    id: string;
+    username: string;
+    disabled?: boolean;
+    admin?: boolean;
+    isStudio?: boolean;
+    inventory?: ({ itemId: string; name: string; description: string; price: number; iconHash: string; } & { amount: number })[];
+    ownedItems?: ShopItem[];
 }
 
-export default function Certification({user, ...props}: {user: User} & React.ImgHTMLAttributes<HTMLImageElement>) {
-    let markSrc = "";
-    switch (true) {
-        case user?.admin:
-            markSrc = "/assets/admin-mark.png";
-            break;
-        case user?.isStudio:
-            markSrc = "/assets/brand-verified-mark.png";
-            break;
-        default:
-            markSrc = "/assets/verified-mark.png";
-    }
+export default function Certification({ user, ...props }: { user: User } & React.ImgHTMLAttributes<HTMLImageElement>) {
+    const [markSrc, setMarkSrc] = React.useState("");
+
+    React.useEffect(() => {
+        if (user?.admin) {
+            setMarkSrc("/assets/admin-mark.png");
+        } else if (user?.isStudio) {
+            setMarkSrc("/assets/brand-verified-mark.png");
+        } else {
+            setMarkSrc("/assets/verified-mark.png");
+        }
+    }, [user]);
 
     return (
         user?.verified ? (
             <img
-                src={markSrc}
+                src={markSrc || "/assets/verified-mark.png"}
                 alt="Verified"
                 {...props}
             />
